90e533fb11aa246da17265707cde41fe11efab7e
Peter Jung <peter@jung.ninja>
2023-06-06 20:12:26 +0200

    more formating

--- GraphINVENT/tools/utils.py
+++ GraphINVENT/tools/utils.py
@@ -2,10 +2,11 @@
 Miscellaneous functions.
 """
 import rdkit
+from collections import Counter
 from rdkit.Chem.rdmolfiles import SmilesMolSupplier
 
 
-def load_molecules(path : str) -> rdkit.Chem.rdmolfiles.SmilesMolSupplier:
+def load_molecules(path: str) -> rdkit.Chem.rdmolfiles.SmilesMolSupplier:
     """
     Reads a SMILES file (full path/filename specified by `path`) and returns the
     `rdkit.Mol` object "supplier".
@@ -17,6 +18,11 @@ def load_molecules(path : str) -> rdkit.Chem.rdmolfiles.SmilesMolSupplier:
     smi_file.close()
 
     # read file
-    molecule_set = SmilesMolSupplier(path, sanitize=True, nameColumn=-1, titleLine=has_header)
+    molecule_set = SmilesMolSupplier(
+        path, sanitize=True, nameColumn=-1, titleLine=has_header
+    )
+    print(
+        "##\nmolecule is none vs not:", Counter(x is None for x in molecule_set), "\n##"
+    )
 
     return molecule_set
